19ca50c7e08ed5e4041aba529acf3a2ab46485dc
melihozmen9 <105754659+melihozmen9@users.noreply.github.com>
2022-06-09 02:25:34 +0300

    Initial Commit

---
+++ closure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  closure
+//
+//  Created by Kullanici on 9.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
